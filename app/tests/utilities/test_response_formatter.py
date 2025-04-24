8548335cb7666c4fcea7f319497d56ffa20af3eb
micah-baize_xom <micah.baize@exxonmobil.com>
2025-04-24 15:35:07 -0500

    finish tests

--- app/tests/utilities/test_response_formatter.py
+++ app/tests/utilities/test_response_formatter.py
@@ -1,12 +1,12 @@
 
 
 from pytest_mock import MockerFixture
-from app.api.models.rules import RuleListResponse
+from app.api.models.rules import RuleListResponse, RuleStats
 from app.utilities.response_formatter import format_list_rules_response
 
 
 def test_format_list_rules_response_inits_response_model_with_correct_params(mocker: MockerFixture):
-    mock_rule_list_response = mocker.Mock(spec=RuleListResponse) #patch('app.utilities.response_formatter.RuleListResponse')
+    mock_rule_list_response = mocker.patch('app.utilities.response_formatter.RuleListResponse', autospec=True)
     entity_types = ["entity0", "entity1"]
     categories_list = ["category0", "category1"]
     categories_dict = {
@@ -34,9 +34,21 @@ def test_format_list_rules_response_inits_response_model_with_correct_params(moc
             categories_list[1] : [ rules[1] ]
         }
     }
-    
+    entity0_rules_by_category = {
+        categories_list[0]: 1,
+        categories_list[1]: 1,
+    }
+    entity1_rules_by_category = {
+        categories_list[1]: 1,
+    }
+  
+    entity0_rule_stats = RuleStats(total_rules=2, rules_by_category=entity0_rules_by_category)
+    entity1_rule_stats = RuleStats(total_rules=1, rules_by_category=entity1_rules_by_category)
+    stats = {
+        entity_types[0] : entity0_rule_stats,
+        entity_types[1] : entity1_rule_stats,
+    }
     result = format_list_rules_response(rules_by_entity)
     
-    # assert isinstance(result, RuleListResponse)
-    mock_rule_list_response.assert_called_with(entity_types=entity_types, categories=categories_dict, rules=rules_by_entity)
-    
+    assert isinstance(result, RuleListResponse)
+    assert mock_rule_list_response.call_args.kwargs == {'entity_types': entity_types, 'categories': categories_dict, 'rules': rules_by_entity, 'stats': stats}
--- app/utilities/response_formatter.py
+++ app/utilities/response_formatter.py
@@ -2,12 +2,28 @@ from app.api.models.rules import RuleListResponse, RuleStats
 
 
 def format_list_rules_response(rules_by_entity: dict) -> RuleListResponse:
+     # Format response
     entity_types = list(rules_by_entity.keys())
     categories = {}
+    stats = {}
 
+    # Get categories and statistics for each entity type
     for entity_type, categories_rules in rules_by_entity.items():
         categories[entity_type] = list(categories_rules.keys())
-    
-       
-    responseModel = RuleListResponse(entity_types=entity_types, categories=categories, rules=rules_by_entity)
+
+        # Calcular estadísticas
+        entity_stats = {
+            "total_rules": 0,
+            "rules_by_category": {}
+        }
+
+        for category, rules_list in categories_rules.items():
+            category_rule_count = len(rules_list)
+            entity_stats["rules_by_category"][category] = category_rule_count
+            entity_stats["total_rules"] += category_rule_count
+
+        stats[entity_type] = RuleStats(total_rules=entity_stats["total_rules"], rules_by_category=entity_stats["rules_by_category"])
+
+
+    responseModel = RuleListResponse(entity_types=entity_types, categories=categories, rules=rules_by_entity, stats=stats)
     return responseModel
