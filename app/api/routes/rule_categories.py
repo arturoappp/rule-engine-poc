cf696ff861ed413ac3c6ac27b56bed9d8f4b380c
micah-baize_xom <micah.baize@exxonmobil.com>
2025-04-29 11:17:59 -0500

    WIP -- updating rule categories endpoint

--- app/api/routes/rule_categories.py
+++ app/api/routes/rule_categories.py
@@ -1,10 +1,13 @@
 from app.api.models.rule_categories import RuleCategoriesRequest, RuleCategoriesResponse
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Depends, status
+
+from app.api.routes.rules import get_rule_service
+from app.services.rule_service import RuleService
 
 router = APIRouter()
 
 @router.post("/rule-categories", response_model=RuleCategoriesResponse, status_code=status.HTTP_200_OK)
-async def update_rule_categories(request: RuleCategoriesRequest):
+async def update_rule_categories(request: RuleCategoriesRequest, service: RuleService = Depends(get_rule_service)):
     """Update rule categories."""
 
 
